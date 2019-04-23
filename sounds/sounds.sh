0011d18a766c096d4915d8c82ade3bbac7dd1fe2
krisshol <krisshol@stud.ntnu.no>
2019-04-23 21:52:56 +0200

    Changed sounds mixer api usage
    Amixer stopped working suddenly, changed to using pulsemixer

--- sounds/sounds.sh
+++ sounds/sounds.sh
@@ -6,14 +6,16 @@
 if [ "$1" != "mute" ]; then {
     #exec --no-startup-id pactl set-sink-volume 0 $1
     pactl set-sink-volume 0 "$1"
-    volume=$(amixer sget Master | awk -F"[][]" '/dB/ { print $2 }')
+    # volume=$(amixer sget Master | awk -F"[][]" '/dB/ { print $2 }')
+    volume=$(pulsemixer --get-volume | awk '{ print $1 }')
     notify-send "Volume:" "$volume"
 } else {
     pactl set-sink-mute 0 toggle
-    toggle=$(amixer sget Master | awk -F"[][]" '/dB/ { print $6 }')
-    if [ "$toggle" = "off" ]; then {
+    #toggle=$(amixer sget Master | awk -F"[][]" '/dB/ { print $6 }')
+    toggle=$(pulsemixer --get-mute)
+    if [ "$toggle" = 0 ]; then {
         notify-send "Volume:" "Muted"
-    } elif [ "$toggle" = "on" ]; then {
+    } elif [ "$toggle" = 1 ]; then {
         notify-send "Volume:" "Unmuted"
     } else {
         notify-send "Volume:" "Something went oopsie"
