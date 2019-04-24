a1ed1e6fbeed9f4d414d8a6be2069c99e8efa074
krisshol <krisshol@stud.ntnu.no>
2019-04-25 00:52:39 +0200

    Sounds should work with bluetooth on and off now
    Very possible that if there's more output sinks than one
    (eg. HDMI, laptop, other) that a legit id will not be chosen.
    I've not tested it yet as it's not an immediate use-case

--- sounds/sounds.sh
+++ sounds/sounds.sh
@@ -3,14 +3,21 @@
 # krisshol@stud.ntnu.no
 
 sink_id=0
-bluetooth_state=($(pactl list short | grep "bluez_sink" | awk  '{ print $1,$7 }'))
-
+bluetooth_state=($(pactl list sinks short | grep "bluez_sink" | awk  '{ print $1,$7 }'))
+echo "${#bluetooth_state[@]}"
 # If bluetooth sink is running:
-for (( i=0; i < ${#bluetooth_state[@]}; i++)); do {
-    if [ "${bluetooth_state[$i]}" == "RUNNING" ]; then {
-        sink_id="${bluetooth_state[i-1]}"
-    } fi
-} done
+if [[ ${#bluetooth_state[@]} -gt 0 ]]; then {
+    for (( i=0; i < ${#bluetooth_state[@]}; i++)); do {
+        if [[ "${bluetooth_state[$i]}" -eq "RUNNING" ]] || 
+           [[ "${bluetooth_state[$i]}" -eq "SUSPENDED" ]]; then {
+            sink_id="${bluetooth_state[i-1]}"
+            echo "bluethooth id: $sink_id"
+        } fi
+    } done
+} else {
+    sink_id=$(pactl list sinks short | awk '{ print $1 }')
+    echo "other: $sink_id"
+} fi
 
 if [ "$1" != "mute" ]; then {
     pactl set-sink-volume "$sink_id" "$1"
@@ -19,9 +26,9 @@ if [ "$1" != "mute" ]; then {
 } else {
     pactl set-sink-mute "$sink_id" toggle
     toggle=$(pulsemixer --get-mute)
-    if [ "$toggle" = 0 ]; then {
+    if [ "$toggle" = 1 ]; then {
         notify-send "Volume:" "Muted"
-    } elif [ "$toggle" = 1 ]; then {
+    } elif [ "$toggle" = 0 ]; then {
         notify-send "Volume:" "Unmuted"
     } else {
         notify-send "Volume:" "Something went oopsie"
