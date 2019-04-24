0a6d1fafb45e12cb9fded45ead71a49596d69816
krisshol <krisshol@stud.ntnu.no>
2019-04-24 20:38:32 +0200

    Detect bluetooth when status RUNNING

--- sounds/sounds.sh
+++ sounds/sounds.sh
@@ -2,16 +2,22 @@
 
 # krisshol@stud.ntnu.no
 
+sink_id=0
+bluetooth_state=($(pactl list short | grep "bluez_sink" | awk  '{ print $1,$7 }'))
+
+# If bluetooth sink is running:
+for (( i=0; i < ${#bluetooth_state[@]}; i++)); do {
+    if [ "${bluetooth_state[$i]}" == "RUNNING" ]; then {
+        sink_id="${bluetooth_state[i-1]}"
+    } fi
+} done
 
 if [ "$1" != "mute" ]; then {
-    #exec --no-startup-id pactl set-sink-volume 0 $1
-    pactl set-sink-volume 0 "$1"
-    # volume=$(amixer sget Master | awk -F"[][]" '/dB/ { print $2 }')
+    pactl set-sink-volume "$sink_id" "$1"
     volume=$(pulsemixer --get-volume | awk '{ print $1 }')
     notify-send "Volume:" "$volume"
 } else {
-    pactl set-sink-mute 0 toggle
-    #toggle=$(amixer sget Master | awk -F"[][]" '/dB/ { print $6 }')
+    pactl set-sink-mute "$sink_id" toggle
     toggle=$(pulsemixer --get-mute)
     if [ "$toggle" = 0 ]; then {
         notify-send "Volume:" "Muted"
